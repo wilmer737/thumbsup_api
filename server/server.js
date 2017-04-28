f3c0104c10bfc605c312d9eebb15bd524680fe51
Wilmer <pollos737@outlook.com>
2017-04-27 23:06:17 -0700

    minor change to server.js

--- server/server.js
+++ server/server.js
@@ -7,14 +7,6 @@ const {Submission} = require('./models/submission');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(function (req, res, next) {
-    if (req.headers['x-forwarded-proto'] === 'https') {
-        res.redirect('http://' + req.hostname + req.url);
-    } else {
-        next();
-    }
-});
-
 app.use(bodyParser.json());
 
 app.post('/submission', (req, res) => {
@@ -30,6 +22,13 @@ app.post('/submission', (req, res) => {
     });
 });
 
+app.use(function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] === 'https') {
+        res.redirect('http://' + req.hostname + req.url);
+    } else {
+        next();
+    }
+});
 
 app.use(express.static('app'));
 
